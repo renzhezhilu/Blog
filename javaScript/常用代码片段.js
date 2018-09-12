dee5fa60c4a35cc6b076d7870ae0ee603e438c09
renzhezhilu <727314374@qq.com>
2018-09-12 20:30:43 +0800

    Update 常用代码片段.js

--- javaScript/常用代码片段.js
+++ javaScript/常用代码片段.js
@@ -16,14 +16,15 @@ parseFloat("4.3fd034") //4.3
 1.335.toFixed(2) //1.33  错误
 //解决精度问题
 var toFixed_ok =function(value,num){
-  value+""
-  let dot = (value.length)-(value.indexOf("."))-2
+  value-0
   let int_num ="";
   for(let i =0;i<num;i++){
     int_num=int_num+"0"
   }
-  int_num="1"+int_num;
-  return Math.round(value*int_num)/int_num
-  //return ((value*int_num).toFixed(num))/int_num
+  let int_num2="1"+int_num;
+  let jg = Math.round(value*int_num2)/int_num2
+  if (typeof jg === 'number' && jg%1 === 0) return +((jg+"")+int_num)
+  else return jg
+  
 }
 
