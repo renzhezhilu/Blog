ffd2a82cba43492b829754dc8ffa909d07d4eba4
renzhezhilu <727314374@qq.com>
2018-10-26 14:42:44 +0800

    Create pid菜单递归.js

---
+++ javaScript/jsToJson/pid菜单递归.js
@@ -0,0 +1,28 @@
+
+
+var navJson = [
+    {"ID":"1","DeptNo":"01","DeptName":"总公司","ParentID":"0"},
+    {"ID":"2","DeptNo":"0101","DeptName":"深圳酷泰丰科技有限公司","ParentID":"1"},
+    {"ID":"3","DeptNo":"0102","DeptName":"深圳市亚夏科技有限公司","ParentID":"1"},
+    {"ID":"4","DeptNo":"0103","DeptName":"深圳市奥克多普科技有限公司","ParentID":"1"},
+    {"ID":"5","DeptNo":"0104","DeptName":"深圳市思柏德科技有限公司","ParentID":"1"},
+    {"ID":"6","DeptNo":"010101","DeptName":"总经办","ParentID":"2"},
+    {"ID":"7","DeptNo":"010102","DeptName":"研发中心","ParentID":"2"},
+    {"ID":"8","DeptNo":"01010201","DeptName":"软件部","ParentID":"7"},
+    {"ID":"9","DeptNo":"0101020101","DeptName":"软件项目组","ParentID":"8"},
+    {"ID":"10","DeptNo":"0101020102","DeptName":"应用组","ParentID":"8"},
+    {"ID":"11","DeptNo":"0101020103","DeptName":"驱动组","ParentID":"8"},
+    {"ID":"12","DeptNo":"0101020104","DeptName":"通讯组","ParentID":"8"},
+    {"ID":"13","DeptNo":"0101020105","DeptName":"工具组","ParentID":"8"},
+    {"ID":"14","DeptNo":"01010202","DeptName":"硬件部","ParentID":"7"},
+    {"ID":"15","DeptNo":"0101020201","DeptName":"射频组","ParentID":"14"},
+    {"ID":"16","DeptNo":"0101020202","DeptName":"基带组","ParentID":"14"},
+    {"ID":"17","DeptNo":"0101020203","DeptName":"layout组","ParentID":"14"},
+    {"ID":"18","DeptNo":"0101020204","DeptName":"部品组","ParentID":"14"},
+    {"ID":"19","DeptNo":"0101020205","DeptName":"BOM组","ParentID":"14"},
+    ]
+    
+    
+    
+    function 
+    
