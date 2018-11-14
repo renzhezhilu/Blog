94b0f4b6b6bab76479c37962f02c52237ae653bb
renzhezhilu <727314374@qq.com>
2018-11-14 11:23:25 +0800

    Update 基础-fs模块.js

--- node/基础-fs模块.js
+++ node/基础-fs模块.js
@@ -1,4 +1,4 @@
-//////////////////////////////////////////文件
+/////////////////////////////////////////////////////////////////////文件
 
 //*******************读取*******************//
 ////fs.readFile('路径','编码','模式',回调(error,data){...})
@@ -30,6 +30,89 @@ fs.exists('./123.json', function (exists) {
 //同步
 var file = './http';
 console.log( fs.existsSync(file) ? file+"存在" : file+"不存在" );
+
+//*******************监听*******************//
+
+
+
+
+
+
+
+
+/////fs.watchFile('文件','参数(对象)',回调(curr, prev){...})
+
+//变化后curr，变化前prev
+//用fs.watch(...)性能更好
+fs.watchFile('./123.json', {interval: 2000 }, (curr, prev) => {
+    console.log('curr:',curr,'prev:',prev);
+});
+// 取消监听
+setTimeout(() => {
+fs.unwatchFile(__dirname+'/123.json');
+}, 5000);
+
+/*
+curr:
+{
+    atime:Wed Nov 14 2018 10:32:59 GMT+0800 (China Standard Time) {}
+    atimeMs:1542162779505.3528
+    birthtime:Sat Nov 10 2018 10:32:14 GMT+0800 (China Standard Time) {}
+    birthtimeMs:1541817134609.5735
+    blksize:4194304
+    blocks:8
+    ctime:Wed Nov 14 2018 10:32:59 GMT+0800 (China Standard Time) {}
+    ctimeMs:1542162779484.097
+    dev:16777222
+    gid:20
+    ino:8597644239
+    mode:33188
+    mtime:Wed Nov 14 2018 10:32:59 GMT+0800 (China Standard Time) {}
+    mtimeMs:1542162779484.097
+    nlink:1
+    rdev:0
+    size:12100
+    uid:501
+}
+prev:
+{
+    atime:Wed Nov 14 2018 10:32:50 GMT+0800 (China Standard Time) {}
+    atimeMs:1542162770335.4802
+    birthtime:Sat Nov 10 2018 10:32:14 GMT+0800 (China Standard Time) {}
+    birthtimeMs:1541817134609.5735
+    blksize:4194304
+    blocks:8
+    ctime:Wed Nov 14 2018 10:31:52 GMT+0800 (China Standard Time) {}
+    ctimeMs:1542162712693.218
+    dev:16777222
+    gid:20
+    ino:8597644239
+    mode:33188
+    mtime:Wed Nov 14 2018 10:31:52 GMT+0800 (China Standard Time) {}
+    mtimeMs:1542162712693.218
+    nlink:1
+    rdev:0
+    size:43
+    uid:501
+}
+*/
+
+//更复杂点
+const watchcall = function(curr, prev){
+    if(Date.parse(prev.ctime) == 0){
+        console.log("123.json被创建");
+    }else if(Date.parse(curr.ctime) == 0){
+        console.log("123.json被删除");
+    }else if(Date.parse(prev.mtime) != Date.parse(curr.mtime)){
+        console.log("123.json被修改");
+    }
+}
+fs.watchFile('./123.json', {interval: 1000 }, watchcall);
+ setTimeout(() => {
+    fs.unwatchFile(__dirname+'/123.json',watchcall);
+    console.log("取消监听");
+ }, 50000);
+
 //*******************重命名*******************//
 
 
@@ -45,6 +128,8 @@ fs.writeFile("333.txt","33333",(err)=>{
 })
 ////同步
 fs.writeFileSync("666.txt","666666",'utf8');
+////error
+//Error: ENOENT: no such file or directory 
 
 //*******************追加*******************//
 
@@ -63,10 +148,31 @@ fs.writeFileSync("666.txt","666666",'utf8');
 
 
 
-//////////////////////////////////////////文件夹
+/////////////////////////////////////////////////////////////////////文件夹
 
 //*******************读取*******************//
+//当前路径'./' 当前运行js所在目录 '__dirname'
+fs.readdir(__dirname,(err,jg)=>{
+    if (err) throw err;
+    jg.forEach((item)=>{
+        console.log(item);
+    })
+})
+//同步
+console.log(
+    '返回数组：%s',
+    fs.readdirSync('./')
+);
 
+
+
+//*******************新建*******************//
+//fs.mkdir(目录名称,权限,回调(err){...})
+fs.mkdir('./a',0777, function (err) {
+    if (err) throw err;
+});
+//同步
+fs.mkdirSync('./b',0777);
 
 //*******************重命名*******************//
 
@@ -87,8 +193,44 @@ fs.writeFileSync("666.txt","666666",'utf8');
 //*******************覆盖*******************//
 
 
+/////////////////////////////////////////////////////////////////////判断
+//判断是文件还是目录
+fs.stat('./123.json', (err, stats) => {
+    if (err) throw err;
+    console.log(
+        stats.isFile(),
+        stats.isDirectory(),
+        JSON.stringify(stats)
+    );
+}) 
+/* 
+true false
+{
+    "dev": 16777222,
+    "mode": 16877,
+    "nlink": 10,
+    "uid": 501,
+    "gid": 20,
+    "rdev": 0,
+    "blksize": 4194304,
+    "ino": 8597475163,
+    "size": 320,
+    "blocks": 0,
+    "atimeMs": 1542160887871.3687,
+    "mtimeMs": 1542158748198.276,
+    "ctimeMs": 1542158748198.276,
+    "birthtimeMs": 1541811260036.9626,
+    "atime": "2018-11-14T02:01:27.871Z",
+    "mtime": "2018-11-14T01:25:48.198Z",
+    "ctime": "2018-11-14T01:25:48.198Z",
+    "birthtime": "2018-11-10T00:54:20.037Z"
+}
+*/
 
-//////////////////////////////////////////实用函数
+
+/////////////////////////////////////////////////////////////////////实用函数
+
+
 
 
 
