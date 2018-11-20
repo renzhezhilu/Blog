d3511c3c859189badeb17dd9d8088600d01a737a
renzhezhilu <727314374@qq.com>
2018-11-20 11:11:15 +0800

    Update 调试-console.js

--- javaScript/jsBasic/调试-console.js
+++ javaScript/jsBasic/调试-console.js
@@ -1,1 +1,26 @@
-//https://segmentfault.com/a/1190000002511877
+//信息提示
+console.log("普通信息") 
+console.warn("警告")
+console.error("错误")
+
+//一组提示
+console.group("组提示开始")
+console.warn("内容01")
+console.log("内容02") 
+console.groupEnd("结束")
+
+//执行次数
+var c=0;
+while(c<5){
+  console.count("执行了");
+  c++;
+}
+
+//执行时间
+console.time("是我")
+var a = [];
+for(var i=0; i<1e6; i++){
+  a.push(i);
+}
+console.timeEnd("是我")
+
