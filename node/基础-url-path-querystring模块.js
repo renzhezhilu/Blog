62dc87b094bb79d342d93c9465d80906aa41ae41
renzhezhilu <727314374@qq.com>
2018-11-27 15:03:28 +0800

    Update 基础-url-path-querystring模块.js

--- node/基础-url-path-querystring模块.js
+++ node/基础-url-path-querystring模块.js
@@ -1,4 +1,5 @@
 //////////////////// url模块
+var url = require("url");
 
 //url字符串->url对象
 //参数parseQueryString生成query对象而不是'id=123&name=rzzl'
@@ -34,4 +35,60 @@ console.log(
 
 
 //////////////////// path模块
+var path = require("path");
+
 //规范化的路径格式
+console.log(
+  path.normalize('http:///img.jandan.net/////news///2018///07/8271a4f27b0e62d9c327377a73917edb.jpg'),
+  //http:/img.jandan.net/news/2018/07/8271a4f27b0e62d9c327377a73917edb.jpg
+  path.normalize(`${__dirname}/1.json`) 
+  // /Users/mac/Desktop/fs/1.json
+);
+
+//合并路径
+var p1 = 'congif/',
+    p2 = '/src/ff',
+    p3 = '../icon.png'; // 向上一层自动去除/ff
+console.log(
+  path.join(p1,p2,p3)   //congif/src/icon.png
+);
+
+//绝对路径
+console.log(
+  path.resolve('json/1.json') == `${__dirname}/json/1.json`,  // true 
+  path.resolve('json/1.json')                                 // /Users/mac/Desktop/fs/json/1.json
+);
+
+//相对路径
+console.log(
+  path.relative( 'json/pic/01/03/1.json','json' ) // ../../../..
+);
+
+//获取目录名
+console.log(
+  path.dirname('/json/src01/0903.json') ///json/src01
+);
+
+//获取文件名
+console.log(
+  path.basename('http://ws1.sinaimg.cn/mw600/006g87eSgy1fxla01gdumj30hk0qoacc.jpg','.jpg'), 
+  //006g87eSgy1fxla01gdumj30hk0qoacc
+  path.basename('http://ws1.sinaimg.cn/mw600/006g87eSgy1fxla01gdumj30hk0qoacc.jpg') 
+  //006g87eSgy1fxla01gdumj30hk0qoacc.jpg
+);
+
+//获取扩展名，无则返空
+console.log(
+  path.extname('http://ws1.cn/001111.jpg') //.jpg
+);
+
+//文件分隔符&目录分隔符
+console.log(
+  path.sep,       //   /   window是'\\', Unix是'/'
+  path.delimiter  //   :   window是';', Unix中是':'
+);
+
+
+
+
+
