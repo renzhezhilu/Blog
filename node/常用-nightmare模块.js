e2de3eaad9405c7e051f5a4b92fc0d0e174c043d
renzhezhilu <727314374@qq.com>
2018-11-29 22:07:50 +0800

    Update 常用-nightmare模块.js

--- node/常用-nightmare模块.js
+++ node/常用-nightmare模块.js
@@ -1,1 +1,7 @@
 
+goto(url[,headers]) url为你要跳转的网站url
+wait(selector) 等待某个dom元素出现
+type(selector[,text]) 在selector元素中输入text文本
+click(selector) 点击某个dom元素
+evaluate(fn[,agr1,agr2,...]) 在客户端注入JS脚本并执行 也就是你自己要封装数据的代码
+end() 执行完成，等待对数据的处理
