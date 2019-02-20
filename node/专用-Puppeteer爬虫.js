d4bb863e36f733651e4826f403c7bf54690b39dc
renzhezhilu <727314374@qq.com>
2019-02-20 19:33:58 +0800

    Create 专用-Puppeteer爬虫.js

---
+++ node/专用-Puppeteer爬虫.js
@@ -0,0 +1,26 @@
+//文档-> https://pptr.dev/
+
+//打开浏览器
+const browser = await puppeteer.launch({headless: false})
+
+//关闭浏览器
+
+//新开标签
+//关闭标签
+
+//等待
+
+/////////输入
+//点击
+//双击
+//键盘输入
+//滚轮
+//右键
+
+
+/////////注入
+//原生js
+
+//jquery
+
+
