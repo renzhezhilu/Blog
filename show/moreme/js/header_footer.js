a14d6f71296e69fe74f359f12922326b54d70e0d
renzhezhilu <renzhezhilu@yahoo.com>
2020-07-19 23:36:35 +0800

    gg

---
+++ show/moreme/js/header_footer.js
@@ -0,0 +1,358 @@
+$(document).ready(function() {
+
+    let header = ` <footer>
+    <div class="box">
+        <div class="nav">
+            <div class="navbox">
+                <ul>
+                    <h5>
+                        Product category
+                    </h5>
+                    <li>
+                        <a href="">AI skin tester</a>
+                    </li>
+                    <li>
+                        <a href="">PH detector</a>
+                    </li>
+                    <li>
+                        <a href="">AI smart mirror</a>
+                    </li>
+                    <li>
+                        <a href="">AI skin tester</a>
+                    </li>
+                    <li>
+                        <a href="">Isolation goggles</a>
+                    </li>
+                    <li>
+                        <a href="">Isolation mask 0.4</a>
+                    </li>
+                    <li>
+                        <a href="">Isolation mask 0.2</a>
+                    </li>
+                    <li>
+                        <a href="">Isolation goggles</a>
+                    </li>
+                    <li>
+                        <a href="">Children's isolation mask</a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <h5>
+                        Service
+                    </h5>
+                    <li>
+                        <a href="">Operate the video</a>
+                    </li>
+                    <li>
+                        <a href="">Data privacy policy</a>
+                    </li>
+                    <li>
+                        <a href="">General terms and </a>
+                    </li>
+                    <li>
+                        <a href="">Conditions</a>
+                    </li>
+                    <li>
+                        <a href="">PH detector</a>
+                    </li>
+                </ul>
+                <ul>
+                    <h5>
+                        News
+                    </h5>
+                    <li>
+                        <a href="">Company</a>
+                    </li>
+                    <li>
+                        <a href="">Expo</a>
+                    </li>
+                </ul>
+
+
+                <ul>
+                    <h5>
+                        About
+                    </h5>
+                    <li>
+                        <a href="">Development path</a>
+                    </li>
+                    <li>
+                        <a href="">Our team</a>
+                    </li>
+                    <li>
+                        <a href="">Certifications</a>
+                    </li>
+                    <li>
+                        <a href="">R&D story</a>
+                    </li>
+
+                </ul>
+
+                <ul>
+                    <h5>
+                        Social platforms
+                    </h5>
+                    <li>
+                        <a href="">facebook</a>
+                    </li>
+                    <li>
+                        <a href="">Instagram</a>
+                    </li>
+                    <li>
+                        <a href="">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="">Youtube</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="other">
+                <div class="but m-b-10">Recruitment of dealers</div>
+                <div class="info">
+                    <div class="type">
+                        <p>Contact:</p>
+                        <p>Phone:</p>
+                        <p>Email:</p>
+                        <p>Address:</p>
+                    </div>
+                    <div class="con">
+                        <p>Alice</p>
+                        <p>0086-13418598358</p>
+                        <p>alice@djmiot.com</p>
+                        <p style="line-height: 22px;">501, Beijing Technology Innovation Building, Yuexing 4 road, Nanshan District, Shenzhen, Guangdong province, China.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bot">
+            <p>
+                Copyright © 2020 djmiotInc. All rights reserved
+            </p>
+            <div class="lua">
+                Language： English
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<header class="white">
+
+    <div id="header" class="box">
+
+        <div class="logo">
+            <a href="./index.html">
+                <img src="./images/logo.png" width="150" alt="" />
+            </a>
+            <div class="open_mune_but">
+                <i class="iconfont iconzuhuhoutaiguanlitubiao_- open"></i>
+                <i class="iconfont iconclose close"></i>
+            </div>
+        </div>
+        <ul class="nav">
+            <li>
+                <a href="./page_cplist.html" class="white on tester">Tester</a>
+            </li>
+            <li>
+                <a href="./page_cplist02.html" class="white on shield">Face shield</a>
+            </li>
+
+            <li>
+                <a href="./page_about.html" class="white">About us</a>
+            </li>
+            <li>
+                <a href="./page_qa.html" class="white">Supplier</a>
+            </li>
+            <li>
+                <a href="./page_new.html" class="white">New</a>
+            </li>
+            <li>
+                <a href="./page_article.html" class="white">Business Solutions</a>
+            </li>
+        </ul>
+        <ul class="contact">
+            <li>
+                <a title="Email" href="" class="white">
+                    <i class="iconfont iconEmail"></i>
+                </a>
+            </li>
+            <li>
+                <a title="Email" href="" class="white">
+                    <i class="iconfont iconins"></i>
+                </a>
+            </li>
+            <li>
+                <a title="Email" href="" class="white">
+                    <i class="iconfont iconTwitter"></i>
+                </a>
+            </li>
+            <li>
+                <a title="Email" href="" class="white">
+                    <i class="iconfont iconfacebook"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <style>
+        .card.sub_mune .block {
+            background-color: rgba(255, 255, 255, 0)
+        }
+
+        .card.sub_mune .sub_mune_box {
+            padding-bottom: 100px;
+        }
+
+        .card.sub_mune.min_card .block {
+            height: 360px;
+        }
+    </style>
+    <div class="card  min_card sub_mune sub_mune01">
+        <div class="sub_mune_box">
+
+            <a href="./cp01.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/home01.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>AI skin tester</h4>
+
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+            <a href="./cp03.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/home02.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>AI skin tester</h4>
+
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+            <a href="./cp02.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/home03.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>PH detector</h4>
+
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+            <a href="./cp04.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/home04.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>FAI skin tester</h4>
+
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+
+
+        </div>
+    </div>
+
+    <div class="card min_card sub_mune sub_mune02">
+        <div class="sub_mune_box ">
+            <a href="./page_cp.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/cp01.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>Face mask</h4>
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+            <a href="./page_cp.html" class="block divtest animated" data-animation="fadeInUp">
+                <div class="pic">
+                    <img src="./images/cp02.png" alt="">
+                </div>
+                <div class="text white">
+                    <h4>EYES COVERP</h4>
+
+                    <span class="but m-t-10">
+                        More
+                    </span>
+                </div>
+            </a>
+
+        </div>
+    </div>
+</header>
+`
+        $('body').append(header)
+
+
+    setTimeout(() => {
+        $(".open_mune_but").click(function(e) {
+            let header = $("#header")
+            if (header.hasClass('phone_open')) {
+                header.removeClass('phone_open')
+            } else {
+                header.addClass("phone_open")
+            }
+        });
+    
+        $(".tester").hover(function() {
+            $(".sub_mune01").css('height', $('.sub_mune01 .sub_mune_box').css('height'));
+        }, function() {
+            $(".sub_mune01").css('height', '0');
+        });
+        $(".sub_mune01").hover(function() {
+            $(".sub_mune01").css('height', $('.sub_mune01 .sub_mune_box').css('height'));
+    
+        }, function() {
+            $(".sub_mune01").css('height', '0');
+        });
+        $(".shield").hover(function() {
+            $(".sub_mune02").css('height', $('.sub_mune02 .sub_mune_box').css('height'));
+        }, function() {
+            $(".sub_mune02").css('height', '0');
+        });
+        $(".sub_mune02").hover(function() {
+            $(".sub_mune02").css('height', $('.sub_mune02 .sub_mune_box').css('height'));
+        }, function() {
+            $(".sub_mune02").css('height', '0');
+        });
+    
+        var isScroll = {
+            init: function(_el) {
+                this.start(_el);
+                $(window).on('scroll', function() {
+                    isScroll.start(_el)
+                });
+            },
+            start: function(_el) {
+                var self = this;
+                $(_el).each(function() {
+                    var _self = $(this);
+                    var isScrollTop = $(window).scrollTop();
+                    var isWindowHeiget = $(window).height() * 0.8;
+                    var _class = $(this).data('animation');
+                    if (isScrollTop + isWindowHeiget > $(this).offset().top) {
+                        _self.addClass(_class);
+                    }
+                });
+            }
+        }
+        isScroll.init('.divtest');
+    
+    }, 500);
+
+
+});
